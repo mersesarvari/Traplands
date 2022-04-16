44bb46aca916c60bae6c03a151bee38c3a2d62ec
Necrozgul <merse.sarvari@gmail.com>
2022-04-16 19:16:14 +0200

    adding data binding to user controls

---
+++ WPFGameTest/MVVM/ViewModel/MultiplayerGameMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFGameTest.MVVM.ViewModel
+{
+    internal class MultiplayerGameMenuViewModel
+    {
+        public MultiplayerGameMenuViewModel()
+        {
+
+        }
+    }
+}
