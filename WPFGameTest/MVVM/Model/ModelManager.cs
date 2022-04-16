f86f801522b9ad15411f560662b37306dbf47ce7
Necrozgul <merse.sarvari@gmail.com>
2022-04-16 19:28:52 +0200

    adding databinding to ModelView

---
+++ WPFGameTest/MVVM/Model/ModelManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFGameTest.Core;
+using WPFGameTest.MVVM.ViewModel;
+
+namespace WPFGameTest.MVVM.Model
+{
+    internal class ModelManager:ObservableObject
+    {
+        public  LobbyViewModel LobbyMV { get; set; }
+        public  MainmenuViewModel MainMenuVM { get; set; }
+        public  MultiplayerGameMenuViewModel MultiplayerGameMenuVM { get; set; }
+        public  MultiplayerGameViewModel MultiplayerGameVM { get; set; }
+        public  SingleplayerGameViewModel SingleplayerGameVM { get; set; }
+
+        private static object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ModelManager()
+        {
+            LobbyMV = new LobbyViewModel();
+            MainMenuVM = new MainmenuViewModel();
+            MultiplayerGameMenuVM = new MultiplayerGameMenuViewModel();
+            MultiplayerGameVM = new MultiplayerGameViewModel();
+            SingleplayerGameVM = new SingleplayerGameViewModel();
+        }
+    }
+}
---
+++ WPFGameTest/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFGameTest.Core;
+
+namespace WPFGameTest.MVVM.ViewModel
+{    
+    internal class MainWindowViewModel:ObservableObject
+    {
+        public LobbyViewModel LobbyMV { get; set; }
+        public MainmenuViewModel MainMenuVM { get; set; }
+        public MultiplayerGameMenuViewModel MultiplayerGameMenuVM { get; set; }
+        public MultiplayerGameViewModel MultiplayerGameVM { get; set; }
+        public SingleplayerGameViewModel SingleplayerGameVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            LobbyMV = new LobbyViewModel();
+            MainMenuVM = new MainmenuViewModel();
+            MultiplayerGameMenuVM = new MultiplayerGameMenuViewModel();
+            MultiplayerGameVM = new MultiplayerGameViewModel();
+            SingleplayerGameVM = new SingleplayerGameViewModel();   
+
+
+        }
+
+
+    }
+}
