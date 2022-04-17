a6093d489da7db533f8923c8cdfce0ce1d47559a
Ben <cptsalt@hotmail.com>
2022-04-17 13:55:35 +0200

    rework Level now it stores the spawn point location and solid + interactable objects

--- WPFGameTest/Models/Level.cs
+++ WPFGameTest/Models/Level.cs
@@ -1,18 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using WPFGameTest.Helpers;
+using WPFGameTest.Renderer;
 
 namespace WPFGameTest.Models
 {
     public class Level
     {
         public int[,] Map { get; private set; }
+        public Vector2 SpawnPoint { get; private set; }
+        public List<Entity> Solids { get; private set; }
+        public List<Entity> Interactables { get; private set; }
 
         public Level(int[,] map)
         {
             Map = map;
+            SpawnPoint = new Vector2();
+            Solids = new List<Entity>();
+            Interactables = new List<Entity>();
+        }
+
+        public void Load(Canvas canvas)
+        {
+            for (int i = 0; i < Map.GetLength(0); i++)
+            {
+                for (int j = 0; j < Map.GetLength(1); j++)
+                {
+                    ObjectType e = (ObjectType)Map[i, j];
+                    SolidObject obj = new SolidObject
+                    (
+                        new Vector2(ObjectData.BLOCK_WIDTH * i, ObjectData.BLOCK_HEIGHT * j),
+                        new Vector2(ObjectData.BLOCK_WIDTH, ObjectData.BLOCK_HEIGHT),
+                        Resource.GetImage(e.ToString()),
+                        true
+                    );
+                    switch (e)
+                    {
+                        case ObjectType.Spawn:
+                            SpawnPoint = new Vector2(obj.Transform.Position.X, obj.Transform.Position.Y - 9);
+                            obj.Tag = "Spawn";
+                            canvas.Children.Add(obj.Element);
+                            break;
+                        case ObjectType.Spike:
+                            obj.Tag = "Trap";
+                            Interactables.Add(obj);
+                            canvas.Children.Add(obj.Element);
+                            break;
+                        case > ObjectType.Grass_First and < ObjectType.Grass_Last:
+                            Solids.Add(obj);
+                            canvas.Children.Add(obj.Element);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
     }
 }
