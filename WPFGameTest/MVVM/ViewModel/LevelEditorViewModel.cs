109cd92078d243a3257d9de16bc44ca242cbaf0f
Ben <cptsalt@hotmail.com>
2022-04-27 14:27:27 +0200

    fix level loading

--- WPFGameTest/MVVM/ViewModel/LevelEditorViewModel.cs
+++ WPFGameTest/MVVM/ViewModel/LevelEditorViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using WPFGameTest.Logic;
 using WPFGameTest.MVVM.Commands;
 using WPFGameTest.MVVM.Services;
 using WPFGameTest.MVVM.Stores;
@@ -12,8 +14,13 @@ namespace WPFGameTest.MVVM.ViewModel
 {
     public class LevelEditorViewModel:ViewModelBase
     {
+        ObservableObject selected;
+
         public ICommand NavigateMainMenuCommand { get; }
         public ICommand SaveAndExitCommand { get; }
+
+        private ILevelEditor logic;
+
         public LevelEditorViewModel(INavigationService mainMenuNavigationService)
         {
             NavigateMainMenuCommand = new NavigateCommand(mainMenuNavigationService);
