00f122adf2ce2cb8eab7e45d7ab87fda4edb52bf
Necrozgul <merse.sarvari@gmail.com>
2022-05-10 17:24:27 +0200

    saving

---
+++ WPFGameTest/Models/RenderData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Models
+{
+    public class RenderData
+    {
+        string Fill { get; set; }
+        Transform Transform { get; set; }
+    }
+}
